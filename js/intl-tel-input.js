76e2aba87d5bfbf162243237f43260e1c71b9242
Miguel Pagola <mipagola@gmail.com>
2024-07-05 12:48:31 +0200

    refactor: extract to function

--- js/intl-tel-input.js
+++ js/intl-tel-input.js
@@ -2,15 +2,17 @@ const telephoneCheck = document.createElement('input');
 telephoneCheck.type = 'tel'
 telephoneCheck.value = ''
 
+const getIp = callback => {
+    fetch("https://ipapi.co/json")
+        .then(res => res.json())
+        .then(data => callback(data.country_code))
+        .catch(() => callback("us"))
+}
+
 const telInputIntl = window.intlTelInput(telephoneCheck, {
     autoPlaceholder: "aggressive",
     initialCountry: "auto",
-    geoIpLookup: callback => {
-        fetch("https://ipapi.co/json")
-            .then(res => res.json())
-            .then(data => callback(data.country_code))
-            .catch(() => callback("us"))
-    },
+    geoIpLookup: getIp,
     utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@23.1.0/build/js/utils.js",
 });
 window.intlTelInput = null
