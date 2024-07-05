d889a4dce6c486e68d98ee89924de1e03f2596f5
Miguel Pagola <mipagola@gmail.com>
2024-07-05 14:21:54 +0200

    refactor: extract the default values to a module

---
+++ js/config.js
@@ -0,0 +1,13 @@
+import { phone, phoneNumberMask } from './intl-tel-input.js'
+
+export const defaults = {
+    address: '81019 Cormier Mall, BQ 11474-0882',
+    email: 'green.smith@acmt.com',
+    companyName: 'A Company Making Things',
+    fullname: 'Green Smith',
+    jobTitle: 'Software Developer',
+    phone: phone(),
+    phoneNumberMask: phoneNumberMask(),
+    profilePicture: 'https://acmt.com/employees/f12645ff6a58e619d096afbbda394794.jpg',
+    website: 'https://acmt.com/'
+};
