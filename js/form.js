ed74eb8241ba763826390992709cdfab3ace3f92
Miguel Pagola <mipagola@gmail.com>
2024-07-09 10:48:05 +0200

    refactor: extract to method

--- js/form.js
+++ js/form.js
@@ -6,6 +6,36 @@ const getUrl = str => {
     return `https://${clean}`
 };
 
+const fillSignature = ({ changed }) => {
+    const { component: { key }, value } = changed;
+
+    const el = document.getElementById(key);
+    if ('homepage' === key) {
+        el.href = getUrl(value)
+        el.innerText = value
+    }
+    else if ('email' === key) {
+        el.href = 'mailto:' + value
+        el.innerText = value
+    }
+    else if ('profile-picture' === key) {
+        el.src = value
+    }
+    else {
+        el.innerText = value
+    }
+};
+
+const initilizeSignature = components =>
+    components.forEach(component => {
+        const { key, placeholder } = component;
+
+        if ('profile-picture' != key) {
+            const el = document.getElementById(key);
+            el.innerText = placeholder;
+        }
+    });
+
 const components = [
     {
         type: 'url',
@@ -117,36 +147,11 @@ const translations = {
 
 Formio.createForm(document.getElementById('formio'), { components }, translations)
     .then(form => {
-        form.on('change', ({ changed }) => {
-            const { component: { key }, value } = changed;
-
-            const el = document.getElementById(key);
-            if ('homepage' === key) {
-                el.href = getUrl(value)
-                el.innerText = value
-            }
-            else if ('email' === key) {
-                el.href = 'mailto:' + value
-                el.innerText = value
-            }
-            else if ('profile-picture' === key) {
-                el.src = value
-            }
-            else {
-                el.innerText = value
-            }
-        });
+        form.on('change', fillSignature);
 
         window.setLanguage = lang => {
             form.language = lang;
         }
 
-        components.forEach(component => {
-            const { key, placeholder } = component;
-
-            if ('profile-picture' != key) {
-                const el = document.getElementById(key);
-                el.innerText = placeholder
-            }
-        })
+        initilizeSignature(components);
     });
