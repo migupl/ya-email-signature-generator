f5cb18bef73b7bd92c26f4eca0235dd8f40929fd
Miguel Pagola <mipagola@gmail.com>
2024-07-22 13:35:16 +0200

    refactor: encapsulate the form events into an anonymous function

--- js/signature.js
+++ js/signature.js
@@ -1,89 +1,91 @@
-const getUrl = str => {
-    const clean = str.trim();
-    if (clean.startsWith('http')) return clean
-    return `https://${clean}`
-};
+const onForm = (() => {
+    const getUrl = str => {
+        const clean = str.trim();
+        if (clean.startsWith('http')) return clean
+        return `https://${clean}`
+    };
 
-const fillSignature = ({ id, value }) => {
-    const el = document.getElementById(id);
-    if (!el) return
-
-    el.parentNode.style.display = value ? '' : 'none'
-
-    if ('homepage' === id) {
-        el.href = getUrl(value)
-        el.innerText = value
-    }
-    else if ('email' === id) {
-        el.href = 'mailto:' + value
-        el.innerText = value
-    }
-    else if ('profile-picture' === id) {
-        el.src = value
-    }
-    else if ('telephone' === id || 'mobile-phone' === id) {
-        el.href = 'tel:' + value.replace(/\s/g, '')
-        el.innerText = value
-    }
-    else if ('a' === el.localName) {
-        el.href = getUrl(value)
-    }
-    else {
-        el.innerText = value
-    }
-};
-
-const change = attribute => value => el => {
-    el.style[attribute] = value
-};
-const layoutChangeTypes = {
-    font: { attribute: 'font', action: change('fontFamily') },
-    'font-size': { attribute: 'font', action: change('fontSize') },
-    'profile-border-radius': {
-        attribute: 'profile-radius',
-        action: value => el => change('borderRadius')(`${value}%`)(el)
-    },
-    'social-color': { attribute: 'social-color', action: change('backgroundColor') },
-    'text-color': { attribute: 'font', action: change('color') },
-    'theme-color': {
-        attribute: 'theme-color',
-        action: value => el => change('img' === el.localName ? 'backgroundColor' : 'color')(value)(el)
-    }
-};
-
-document.addEventListener('form:change-field', ev => {
-    ev.stopPropagation()
-    fillSignature(ev.detail)
-})
-
-document.addEventListener('form:change-style', ev => {
-    const { type, value } = ev.detail;
-    const { attribute, action } = layoutChangeTypes[type];
-
-    ev.stopPropagation()
-    document.querySelectorAll(`[${attribute}]`)
-        .forEach(action(value))
-})
-
-document.addEventListener('form:clean-card', ev => {
-    const { ids, lang } = ev.detail;
-
-    ev.stopPropagation()
-    ids.forEach(id => {
+    const fillSignature = ({ id, value }) => {
         const el = document.getElementById(id);
         if (!el) return
 
-        if ('profile-picture' === id) {
-            el.src = '/assets/empty-profile.png'
+        el.parentNode.style.display = value ? '' : 'none'
+
+        if ('homepage' === id) {
+            el.href = getUrl(value)
+            el.innerText = value
+        }
+        else if ('email' === id) {
+            el.href = 'mailto:' + value
+            el.innerText = value
+        }
+        else if ('profile-picture' === id) {
+            el.src = value
+        }
+        else if ('telephone' === id || 'mobile-phone' === id) {
+            el.href = 'tel:' + value.replace(/\s/g, '')
+            el.innerText = value
+        }
+        else if ('a' === el.localName) {
+            el.href = getUrl(value)
         }
         else {
-            el.parentNode.style.display = 'none'
+            el.innerText = value
         }
-    })
+    };
 
-    messageShowBy(lang)
-    message.style.display = ''
-})
+    const change = attribute => value => el => {
+        el.style[attribute] = value
+    };
+    const layoutChangeTypes = {
+        font: { attribute: 'font', action: change('fontFamily') },
+        'font-size': { attribute: 'font', action: change('fontSize') },
+        'profile-border-radius': {
+            attribute: 'profile-radius',
+            action: value => el => change('borderRadius')(`${value}%`)(el)
+        },
+        'social-color': { attribute: 'social-color', action: change('backgroundColor') },
+        'text-color': { attribute: 'font', action: change('color') },
+        'theme-color': {
+            attribute: 'theme-color',
+            action: value => el => change('img' === el.localName ? 'backgroundColor' : 'color')(value)(el)
+        }
+    };
+
+    const changeStyle = event => {
+        const { type, value } = event.detail;
+        const { attribute, action } = layoutChangeTypes[type];
+
+        event.stopPropagation()
+        document.querySelectorAll(`[${attribute}]`)
+            .forEach(action(value))
+    };
+    const clean = event => {
+        const { ids, lang } = event.detail;
+
+        event.stopPropagation()
+        ids.forEach(id => {
+            const el = document.getElementById(id);
+            if (!el) return
+
+            if ('profile-picture' === id) {
+                el.src = '/assets/empty-profile.png'
+            }
+            else {
+                el.parentNode.style.display = 'none'
+            }
+        })
+
+        messageShowBy(lang)
+        message.style.display = ''
+    };
+    const fill = event => {
+        event.stopPropagation()
+        fillSignature(event.detail)
+    };
+
+    return { changeStyle, clean, fill }
+})();
 
 const onResizePictureProfile = (() => {
     const resize = (() => {
@@ -117,6 +119,11 @@ const messageShowBy = language => {
         message.style.display = language === message.lang ? '' : 'none'
     )
 }
+
+
+document.addEventListener('form:clean-card', onForm.clean)
+document.addEventListener('form:change-field', onForm.fill)
+document.addEventListener('form:change-style', onForm.changeStyle)
 
 document.addEventListener('form:profile-picture:bigger', onResizePictureProfile.bigger)
 document.addEventListener('form:profile-picture:smaller', onResizePictureProfile.smaller)
